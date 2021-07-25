d4e7b5dab1f0e9736c74153abaf6092082b1c5f4
kodai-kato <greatvoyage0122@gmail.com>
2021-07-25 20:43:26 +0900

    Allow request from front production app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3003', 'https://baseball-dictionary.herokuapp.com/'
+    origins 'http://localhost:3003', 'https://baseball-app-75126.web.app/'
 
     resource '*',
       headers: :any,
